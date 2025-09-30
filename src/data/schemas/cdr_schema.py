d3c16de97af09bf126a7d67a564d2ef25b685d33
lhgiang040504 <lhgiang040504@gmail.com>
2025-09-30 18:52:49 +0700

    feat(schemas): add CDR and user schemas

---
+++ src/data/schemas/cdr_schema.py
@@ -0,0 +1,15 @@
+CDRSchema = {
+    "call_id": str,
+    "caller_id": str,
+    "callee_id": str,
+    "call_start_ts": str,
+    "call_end_ts": str,
+    "call_duration": int,
+    "first_cell_id": str,
+    "last_cell_id": str,
+    "caller_imei": str,
+    "caller_imsi": str,
+    "callee_imsi": str,
+    "is_anomaly": int,
+    "anomaly_type": str
+}
---
+++ src/data/schemas/user_schema.py
@@ -0,0 +1,10 @@
+UserSchema = {
+    "user_id": str,
+    "phone_number": str,
+    "imei": str,
+    "imsi": str,
+    "home_cell_id": str,
+    "user_type": str,
+    "creation_date": str,
+    "call_pattern": str
+}
